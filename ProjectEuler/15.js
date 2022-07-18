4bde3e8c30bef3a6cbfa9de42cf5a38f4862104b
Jordi <jordibustos193771@alumnos.info.unlp.edu.ar>
2022-07-17 23:12:33 -0300

    Add [15, 17]

---
+++ ProjectEuler/15.js
@@ -0,0 +1,13 @@
+/*
+Lattice Path
+*/
+
+function latticePaths(gridSize) {
+  let n = factorial(2*gridSize)
+  let d = factorial(gridSize)**2
+
+  return n/d
+}
+
+const factorial = (n) => (n > 0 ? n * factorial(n - 1) : 1);
+console.log(latticePaths(4));
---
+++ ProjectEuler/16.js
@@ -0,0 +1,12 @@
+/*
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^exponent?
+*/
+
+function powerDigitSum(e) {
+  const n = BigInt(Math.pow(2, e))
+  const d = num.toString().split('')
+
+  return digits.reduce((sum, digit) => sum + parseInt(digit), 0)
+}
