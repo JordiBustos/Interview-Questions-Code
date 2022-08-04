0aa450967c223c9734cfeda476e89e11685f1c3b
Jordi <jordibustos193771@alumnos.info.unlp.edu.ar>
2022-08-03 23:31:09 -0300

    Make getArticles recursive

--- Take Home Projects/Wikipedia API/index.js
+++ Take Home Projects/Wikipedia API/index.js
@@ -1,7 +1,7 @@
 const API_URL = 'https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search='
-let reset = false;
+let first = 0;
 var form = document.getElementById('inputForm');
-form.addEventListener('submit', handleForm)
+form.addEventListener('submit', handleForm);
 
 function handleForm(event) {
     event.preventDefault();
@@ -29,37 +29,53 @@ function display(data) {
     document.getElementById('showQuery').innerText = `Your query was: ${data[0]}`
     
     // this is how the api returns it
-    const titles = data[1]; 
-    const links = data[3]
+    const titles = data[1].reverse(); 
+    const links = data[3].reverse();
+    let length = titles.length - 1;
 
-    createArticles(titles, links);
+    createArticles(titles, links, length, first++);
 }
 
-function createArticles(titles, links) {
-    const div = document.getElementById('articles'); 
-    
-    if (reset) {
+const div = document.getElementById('articles');
+
+function createArticles(titles, links, i, first) {
+    if (titles.length == 0 || links.length == 0) return;
+    if (first > 1) {
         document.getElementById('articles').innerHTML = '';
-        reset = !reset
-    } 
-    reset = !reset
-
-    for (let i = 0; i < titles.length; i++) {
-        let newDiv = document.createElement('div')
-
-        let h2 = document.createElement('h2');
-        let title = document.createTextNode(titles[i]);
-        h2.append(title);
-
-        let a = document.createElement('a');
-        let link = document.createTextNode(links[i]);
-        a.append(link);
-        a.href = links[i];
-        a.target = '_blank'
-
-        newDiv.append(h2);
-        newDiv.append(a);
-        newDiv.className = 'singleArticle'
-        div.append(newDiv);
     }
+    
+    let h2 = createSubtitle(titles[i]); 
+    let a = createLink(links[i]);
+
+    let newDiv = createNewDiv(h2, a);
+
+    div.append(newDiv);    
+    i--;
+    createArticles(titles.slice(0, titles.length - 1), links.slice(0, links.length - 1), i)
+}
+
+function createNewDiv(h2, a) {
+    let newDiv = document.createElement('div');
+    newDiv.append(h2); newDiv.append(a);
+    newDiv.className = 'singleArticle';
+
+    return newDiv;
+}
+
+function createSubtitle(title) {
+    let h2 = document.createElement('h2');
+    let newTitle = document.createTextNode(title);
+    h2.append(newTitle);
+
+    return h2;
+}
+
+function createLink(link) {
+    let a = document.createElement('a');
+    let newLink = document.createTextNode(link);
+    a.append(newLink);
+    a.href = link;
+    a.target = '_blank';
+
+    return a
 }
