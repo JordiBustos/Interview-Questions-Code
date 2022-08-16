82575c85d552cf5e7df5d953a75b00307a70fbda
Jordi <jordibustos193771@alumnos.info.unlp.edu.ar>
2022-08-16 17:31:03 -0300

    Add docs

--- Data Structures/03-HashTable/Extra-Hashing/hash_table.py
+++ Data Structures/03-HashTable/Extra-Hashing/hash_table.py
@@ -1,9 +1,18 @@
 class HashTable:
+    """
+        Hash table implementation on python.
+        Methods: set_item, get_item.
+        Private methods: __resize, __hash_function
+    """
     def __init__(self):
         self.table = [None] * 2003
         self.num_items =  0
     
     def set_item(self, key, value):
+        """
+        Receives a key and a value and adds them to the hash table.
+        If the key already exists, appends [[key, value]] to the same place on the table.
+        """
         self.num_items += 1
         load_factor = self.num_items / len(self.table)
 
@@ -17,6 +26,11 @@ class HashTable:
             self.table[i].append([key, value])
 
     def get_item(self, key):
+        """
+        Receives a key and returns the value associated with it.
+        If the hash asociated with key has a collision, find inside self.table[i]
+        the place where self.table[i][j][0] sea la key and return self.table[i][j][1]
+        """
         i = self.__hash_function(key, len(self.table))
         if (not self.table[i]): 
             return None
@@ -26,6 +40,9 @@ class HashTable:
                 return self.table[i][j][1]
 
     def __resize(self):
+        """
+        Private method that resizes the table. Creates a new array with twice the size of the old one.
+        """
         new_table = [0] * (len(self.table) * 2)
         for i in range(len(self.table)):
             if (self.table[i]):
@@ -34,6 +51,9 @@ class HashTable:
         self.table = new_table
 
     def __hash_function(self, key, table_size):
+        """
+        Hash function that receives a key and a table size and returns the hash associated with the key.
+        """
         hashed_key = 17
         random_prime_number = 13
 
@@ -51,4 +71,5 @@ hash_table.set_item('d', 4)
 
 print(hash_table.get_item('a'))
 print(hash_table.get_item('b'))
+
 
