eaef83b13acb0b1d8976512cede018fbe2162d69
Jordi <jordibustos193771@alumnos.info.unlp.edu.ar>
2022-08-19 11:46:42 -0300

    add compute with unequal partitions and some comments

--- Numeric Differentiation/riemann_sum.py
+++ Numeric Differentiation/riemann_sum.py
@@ -3,12 +3,10 @@ import numpy as np
 
 def riemann_sum(f, a, b, rule="left", delta_x=.01):
     """
-    f is the function
-    [a, b] is the interval
-    rule determines if we compute right riemann,left riemann, midpoint riemann
-    rule is left by default
-    delta_x is the width of the partition in the x axis, .01 by default
-    returns the computed riemann sum
+    f is the function.
+    [a, b] is the interval should be .
+    rule determines if we compute right riemann,left riemann, midpoint riemann, is left by default.
+    delta_x is the width of the partition on the x axis, .01 by default.
     """
     acc = 0
     # arange creates an array of evenly spaced numbers between [a, b)
@@ -27,15 +25,30 @@ def riemann_sum(f, a, b, rule="left", delta_x=.01):
         for i in range(n-1):
             acc += f(partition[i+1])
 
-    """
-    We can multiply acc by delta_x at the end because the
-    sum from i to k of n * c is equal to c * the sum from i to k of n
-    in particular the sum of from i = 0 to n-1 * delta_x is equal to delta_x * the sum of from i = 0 to n-1 ...
-    """
+    # We can multiply acc by delta_x at the end because the width of the partition is the same for all x_i
     return acc * delta_x
 
 
-def compute_delta_x(n, a, b):
+def compute_with_unequal_partition(f, delta_x):
+    """
+    Here delta_x is an array [a, x_i, x_i+1, ..., b] and the distance between each x_i is delta_x[i+1] - delta_x[i]
+    can be different for each x_i :)
+    delta_x[0] must be equal to a
+    delta_x[len(delta_x)-1] must be equal to b
+    """
+    acc = 0
+    n = len(delta_x)
+
+    for i in range(n):
+        width = delta_x[i+1] - delta_x[i]
+        acc += f(delta_x[i]) * width
+
+    # f(delta_x[i+1]) for right sum
+    # f((delta_x[i] + delta_x[i+1]) / 2) for midpoint sum
+    return acc
+
+
+def compute_partition(n, a, b):
     """
     If it's needed we can compute the number of wanted partitions
     and then pass the returned value as delta_x in riemann_sum
@@ -45,11 +58,10 @@ def compute_delta_x(n, a, b):
     return int((b-a)/n)
 
 
-a = 1
-b = 100
-def f(x): return x**3
+if __name__ == "__main__":
+    a = 1
+    b = 100
+    def f(x): return x**3
 
-
-rule = "midpoint"
-
-print(riemann_sum(f, a, b, rule))
+    rule = "midpoint"
+    print(riemann_sum(f, a, b, rule))
