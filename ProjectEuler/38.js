310079ea93ea1bfce1a7b76e59c0b33ea0545e54
Jordi <jordibustos193771@alumnos.info.unlp.edu.ar>
2022-10-25 00:27:28 -0300

    Solved  38 project euler

--- ProjectEuler/38.js
+++ ProjectEuler/38.js
@@ -1,5 +1,41 @@
 // https://projecteuler.net/problem=38
 
-const pandigitalMultiples = (n) => {
-  
+function pandigitalMultiples(k) {
+  function getKDigitConcatenatedProduct(num, k) {
+    // returns false if concatenated product is not k digits
+    let concatenatedProduct = num.toString();
+    for (let i = 2; concatenatedProduct.length < k; i++) {
+      concatenatedProduct += num * i;
+    }
+    return concatenatedProduct.length === k ? concatenatedProduct : false;
+  }
+
+  function is1toKPandigital(num, k) {
+    const numStr = num.toString();
+
+    // check if length is not k
+    if (numStr.length !== k) {
+      return false;
+    }
+
+    // check if pandigital
+    for (let i = k; i > 0; i--) {
+      if (numStr.indexOf(i.toString()) === -1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let largestNum = 0;
+  for (let i = 10 ** Math.floor(k / 2) + 1; i >= 1; i--) {
+    const concatenatedProduct = getKDigitConcatenatedProduct(i, k);
+    if (is1toKPandigital(concatenatedProduct, k)) {
+      const number = parseInt(concatenatedProduct, 10);
+      if (number > largestNum) {
+        largestNum = number;
+      }
+    }
+  }
+  return largestNum;
 }
