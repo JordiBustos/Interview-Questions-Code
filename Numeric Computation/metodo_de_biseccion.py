9ff0b43db687f55cee6d54d3b5b86af1e9eef341
Jordi Bustos <88995010+JordiBustos@users.noreply.github.com>
2023-08-30 20:05:28 -0300

    Add files via upload

---
+++ Numeric Computation/metodo_de_biseccion.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+def metodo_de_biseccion(f, a, b, N, TOL):
+    rangea = a
+    rangeb = b
+    for _ in range(1, N+1):
+        p = (a + b) / 2.0
+        graficar_aproximacion(f, p, rangea, rangeb)
+        if (f(p) < 0.01 and f(p) > -0.01) or abs(a - b) / 2.0 < TOL:
+            return p
+        elif f(a) * f(p) < 0:
+            b = p
+        else:
+            a = p
+    return None
+
+def graficar_aproximacion(f, p, a, b):
+    '''
+      Grafica la función f(x) en el intervalo [a, b] y marca el punto p que es el punto medio en la respectiva iteración.
+    '''
+    x = range(a-10, b+10)
+    y = [f(i) for i in x]
+    plt.plot(x, y)
+    plt.plot(p, f(p), 'ro')
+    plt.show()
+
+print(metodo_de_biseccion(lambda x: x**3 + 4 * x**2 - 10, -20, 20, 100, 0.0001))
