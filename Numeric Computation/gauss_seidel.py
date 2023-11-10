ede0370032867cf16a600af53d0352b1284ee401
Jordi Bustos <88995010+JordiBustos@users.noreply.github.com>
2023-11-10 19:44:47 -0300

    generalize gauss seidel to admit any system

--- Numeric Computation/gauss_seidel.py
+++ Numeric Computation/gauss_seidel.py
@@ -1,6 +1,21 @@
 import numpy as np
 
 A = [[8, 3, -3], [-2, -8, 5], [3, 5, 10]]
+b = [14, 5, -8]
+
+
+def create_linear_equations(A, b):
+    n = len(b)
+    equations = []
+
+    for i in range(n):
+        equation = (
+            lambda x, i=i: (b[i] - sum(A[i][j] * x[j] for j in range(n) if j != i))
+            / A[i][i]
+        )
+        equations.append(equation)
+
+    return equations
 
 
 def find_diagonal_coeff(A):
@@ -21,31 +36,28 @@ def is_diag_dominant(A):
     return np.all(diag > off_diag)
 
 
-if is_diag_dominant(A):
-    x1 = x2 = x3 = 0
+if is_diag_dominant(A) and len(A) == len(b):
     epsilon = 0.01
     converged = False
 
-    x_old = np.array([x1, x2, x3])
+    x_old = np.zeros(len(b))
+    x = np.zeros(len(b))
+    equations = create_linear_equations(A, b)
 
     print("Iteration results")
     print(" k,    x1,    x2,    x3 ")
     for k in range(1, 50):
-        x1 = (14 - 3 * x2 + 3 * x3) / 8
-        x2 = (5 + 2 * x1 - 5 * x3) / (-8)
-        x3 = (-8 - 3 * x1 - 5 * x2) / (-5)
-        x = np.array([x1, x2, x3])
-
+        for i in range(len(equations)):
+            x[i] = equations[i](x_old)
         dx = compute_tolerance(x, x_old)
 
-        print("%d, %.4f, %.4f, %.4f" % (k, x1, x2, x3))
+        print("%d," % k + ",".join([f" %.4f" % x[i] for i in range(len(b))]))
         if dx < epsilon:
             converged = True
             print("Converged!")
             break
 
-        # assign the latest x value to the old value
-        x_old = x
+        x_old = np.copy(x)
 
     if not converged:
         print("Not converge, increase the # of iterations")
